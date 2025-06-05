d4a7cf590c35920af06e616a8aab24550ce7234c
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-06-04 20:02:25 -0400

    Broken down the application in different components

---
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-status',
+  templateUrl: './server-status.component.html',
+  styleUrl: './server-status.component.scss',
+  imports: []
+})
+export class ServerStatusComponent {
+
+  currentStatus = 'online';
+
+
+}
---
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tickets',
+  templateUrl: './tickets.component.html',
+  styleUrl: './tickets.component.scss',
+  imports: []
+})
+export class TicketsComponent {
+
+}
