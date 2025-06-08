52bb5e3fe92c1ac0790a5e415a10c183348ff818
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-06-08 14:17:07 -0400

    updated new-ticket component

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 
 @Component({
   selector: 'app-tickets',
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.scss',
-  imports: []
+  imports: [NewTicketComponent]
 })
 export class TicketsComponent {
 
